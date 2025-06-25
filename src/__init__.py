12c125af3500464bb8895e195f656c524bc7d8f1
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2025-06-26 00:48:01 +0100

    Initial commit

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+from .main import run_job, run_jobs
+from .schemas import Job, Jobs, Database, Query
+
+__all__ = [
+    "run_job",
+    "run_jobs",
+    "Job",
+    "Jobs",
+    "Database",
+    "Query",
+]
